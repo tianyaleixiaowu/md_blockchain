03b65ef29c528bc42361b46ca267af2697adfb78
wuwf <272551766@qq.com>
2018-02-26 14:58:36 +0800

    初始化

---
+++ src/main/java/com/mindata/blockchain/MdBlockchainApplication.java
@@ -0,0 +1,12 @@
+package com.mindata.blockchain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdBlockchainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MdBlockchainApplication.class, args);
+	}
+}
---
+++ src/test/java/com/mindata/blockchain/MdBlockchainApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mindata.blockchain;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MdBlockchainApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
